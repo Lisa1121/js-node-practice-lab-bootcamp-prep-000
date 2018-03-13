1a07d6687738191aabd3f3ddf0b231c7661f34a6
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-13 04:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Lisa"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)-9)
+console.log(height-9)
